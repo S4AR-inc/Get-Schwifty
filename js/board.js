61b56c258bf65dd629a833123ea8644d58070ea3
SaarBS <saar1.bs1@gmail.com>
2022-07-03 17:56:45 +0300

    Created board ui generation

--- js/board.js
+++ js/board.js
@@ -1,10 +1,31 @@
 class Board{
 	constructor(){
-		this.board = [
+		this.Board = [
 			[6, null, 7],
 			[3, 2, 8],
 			[4, 5, 1]
 		];
-		this.empty = {x: 1, y: 0};
+		this.Empty = {x: 1, y: 0};
+	}
+
+	generateBoard(uiRoot){
+		this.UIRoot = uiRoot;
+		let table = document.createElement("table");
+		table.classList.add("board");
+		this.Board.forEach((row) => {
+			let tr = document.createElement("tr");
+			row.forEach((value) => {
+				let cell = document.createElement("td");
+				cell.classList("cell");
+				if(value === null){
+					cell.classList.add("empty");
+				}else{
+					cell.innerText(value);
+				}
+				tr.appendChild(cell);
+			});
+			table.appendChild(tr);
+		});
+		this.uiRoot.appendChild(table)
 	}
 }
