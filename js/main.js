e299b24b98b589f305bcd4954121c93dd43e7d67
SaarBS <saar1.bs1@gmail.com>
2022-07-04 11:47:43 +0300

    Made game react to user input

--- js/main.js
+++ js/main.js
@@ -1,12 +1,29 @@
-import { Board } from "./board.js"
+import { Board, DIRECTION } from "./board.js"
+import { deepFreeze, IMECompositionFilter } from "./utils.js"
 
 let GameBoard;
+
+const KEYS_DIRECTION_MAP = deepFreeze([
+	{keys: [ "ArrowLeft",	"KeyA" ], direction: DIRECTION.LEFT	},
+	{keys: [ "ArrowRight",	"KeyD" ], direction: DIRECTION.RIGHT},
+	{keys: [ "ArrowUp",		"KeyW" ], direction: DIRECTION.UP	},
+	{keys: [ "ArrowDown",	"KeyS" ], direction: DIRECTION.DOWN	},
+]);
 
 Load();
 
 function Load() {
 	GameBoard = new Board();
-	GameBoard.generateBoard(document.querySelector("body>main"))
+	GameBoard.generateBoard(document.querySelector("body>main"));
+	document.addEventListener("keydown", (keyboardEvent) => {
+		IMECompositionFilter((keyboardEvent) => {
+			let direction = KEYS_DIRECTION_MAP.find(mapping => mapping.keys.includes(keyboardEvent.code))?.direction;
+			if(direction === undefined){
+				return;
+			}
+			GameBoard.MoveByDirection(direction);
+		},keyboardEvent);
+	});
 }
 
 const exports = { GameBoard, Load };
